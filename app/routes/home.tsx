f1e6cb66a63601e926eee4870fbd58a4cddfa6c2
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2025-12-18 16:04:26 +0530

    development bug fix

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,20 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Rsumind" },
+    { name: "description", content: "Smart frrback for your dream job " },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg)] bg-cover">
+      <section className="main-section">
+          <div className="page-heading">
+              <h1>Track your application & resume rating</h1>
+              <h2>Review your submission and check AI-powered feedback</h2>
+          </div>
+      </section>
+
+      </main>
 }
